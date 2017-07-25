9a12ac2b2361db5c768dba627e8da5bc84f52793
xu0o0 <hqcat6@gmail.com>
2017-07-26 03:26:19 +0800

    finish lab2

--- labcodes/lab2/kern/mm/pmm.c
+++ labcodes/lab2/kern/mm/pmm.c
@@ -380,6 +380,17 @@ get_pte(pde_t *pgdir, uintptr_t la, bool create) {
     }
     return NULL;          // (8) return page table entry
 #endif
+    pde_t *pdep = &pgdir[PDX(la)];
+    if (!(*pdep & PTE_P)) {
+        struct Page *p;
+        if (!create || (p = alloc_page()) == NULL)
+            return NULL;
+        set_page_ref(p, 1);
+        intptr_t pa = page2pa(p);
+        memset(KADDR(pa), 0, PGSIZE);
+        *pdep = pa | PTE_P | PTE_U | PTE_W;
+    }
+    return (pte_t *)KADDR(PDE_ADDR(*pdep)) + PTX(la);
 }
 
 //get_page - get related Page struct for linear address la using PDT pgdir
@@ -425,6 +436,15 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
                                   //(6) flush tlb
     }
 #endif
+    struct Page *p;
+    if ((*ptep & PTE_P)) {
+        p = pte2page(*ptep);
+        page_ref_dec(p);
+        if (page_ref(p) == 0)
+            pmm_manager->free_page(p);
+        *ptep = 0;
+        tlb_invalidate(pgdir, la);
+    }
 }
 
 //page_remove - free an Page which is related linear address la and has an validated pte
